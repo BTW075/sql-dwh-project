0df3611677591c126a821b1fe3fa475f652a12bc
BTW075 <116366078+BTW075@users.noreply.github.com>
2026-05-06 13:34:38 +0530

    Fix SQL syntax and update view definitions

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -12,6 +12,8 @@ Usage:
 =====================================================================================
 */
 
+USE CATALOG DataWarehouse;
+
 CREATE VIEW  gold.dim_customers AS
 SELECT 
 ROW_NUMBER() OVER (ORDER BY cst_id) AS cusotmer_key,
@@ -30,9 +32,8 @@ FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
-ON ci.cst_key = la.cid
+ON ci.cst_key = la.cid;
 
-GO
 CREATE VIEW gold.dim_products AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
@@ -47,15 +48,14 @@ SELECT
     pn.prd_line     AS product_line,
     pn.prd_start_dt AS start_date
 FROM silver.crm_prd_info pn
-LEFT JOIN silver.erp_px_cat_g1v2 pc
+LEFT JOIN silver.erp_px_cat_g1v1 pc
     ON pn.cat_id = pc.id
-WHERE pn.prd_end_dt IS NULL
+WHERE pn.prd_end_dt IS NULL;
 
-GO
 CREATE VIEW gold.fact_sales AS
 SELECT 
 		sd.sls_ord_num AS order_number,
-		pr.products_key,
+		pr.product_key,
 		cu.cusotmer_key,
 		sd.sls_order_dt AS order_date,
 		sd.sls_ship_dt AS shipping_date,
@@ -67,4 +67,4 @@ silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
 ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu 
-ON sd.sls_cust_id = cu.customer_id 
+ON sd.sls_cust_id = cu.customer_id;
