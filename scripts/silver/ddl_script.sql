9397dea7a5e889fa45aabfd059f001c04bebc211
BTW075 <116366078+BTW075@users.noreply.github.com>
2026-05-06 13:32:17 +0530

    Refactor DDL script for table creation and dropping

--- scripts/silver/ddl_script.sql
+++ scripts/silver/ddl_script.sql
@@ -10,41 +10,37 @@ Script Purpose:
 =======================================================================
 */
 
-IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
-	DROP TABLE silver.crm_cust_info
+USE CATALOG DataWarehouse;
+
+DROP TABLE IF EXISTS silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info(
 cst_id INT,
-cst_key NVARCHAR(50),
-cst_firstname NVARCHAR(50),
-cst_lastname NVARCHAR(50),
-cst_marital_status NVARCHAR(50),
-cst_gndr NVARCHAR(50),
+cst_key STRING,
+cst_firstname STRING,
+cst_lastname STRING,
+cst_marital_status STRING,
+cst_gndr STRING,
 cst_create_date DATE,
-dwh_create_date DATETIME2 DEFAULT GETDATE()
+dwh_create_date TIMESTAMP
 );
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-	DROP TABLE silver.crm_prd_info;
-
---statement to be added before each DDL 
+DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info(
 prd_id INT,
-cat_id NVARCHAR(50),
-prd_key NVARCHAR(50),
-prd_nm NVARCHAR(50),
+cat_id STRING,
+prd_key STRING,
+prd_nm STRING,
 prd_cost INT,
-prd_line NVARCHAR(50),
+prd_line STRING,
 prd_start_dt DATE,
 prd_end_dt DATE,
-dwh_create_date DATETIME2 DEFAULT GETDATE()
+dwh_create_date TIMESTAMP
 );
 
-
-IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
-	DROP TABLE silver.crm_sales_details;
+DROP TABLE IF EXISTS silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details(
-sls_ord_num NVARCHAR(50),
-sls_prd_key NVARCHAR(50),
+sls_ord_num STRING,
+sls_prd_key STRING,
 sls_cust_id INT,
 sls_order_dt DATE,
 sls_ship_dt DATE,
@@ -52,26 +48,29 @@ sls_due_dt DATE,
 sls_sales INT,
 sls_quantity INT,
 sls_price INT,
-dwh_create_date DATETIME2 DEFAULT GETDATE()
+dwh_create_date TIMESTAMP
 );
 
+DROP TABLE IF EXISTS silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12(
-cid NVARCHAR(50),
+cid STRING,
 bdate DATE,
-gen NVARCHAR(50),
-dwh_create_date DATETIME2 DEFAULT GETDATE()
+gen STRING,
+dwh_create_date TIMESTAMP
 );
 
+DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101(
-cid NVARCHAR(50),
-cntry NVARCHAR(50),
-dwh_create_date DATETIME2 DEFAULT GETDATE()
+cid STRING,
+cntry STRING,
+dwh_create_date TIMESTAMP
 );
 
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v1;
 CREATE TABLE silver.erp_px_cat_g1v1(
-id NVARCHAR(50),
-cat NVARCHAR(50),
-subcat NVARCHAR(50),
-maintenance NVARCHAR(50),
-dwh_create_date DATETIME2 DEFAULT GETDATE()
+id STRING,
+cat STRING,
+subcat STRING,
+maintenance STRING,
+dwh_create_date TIMESTAMP
 );
